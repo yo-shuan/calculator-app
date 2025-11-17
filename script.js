f1035cf9de09476d9f16283c9b34f72459e4232b
Shuan <ppk060218@gmail.com>
2025-11-17 17:25:41 +0800

    Initial calculator with basic operations

---
+++ script.js
@@ -0,0 +1,43 @@
+let display = document.getElementById('display');
+let currentInput = '0';
+
+function updateDisplay() {
+    display.textContent = currentInput;
+}
+
+function appendToDisplay(value) {
+    if (currentInput === '0' && value !== '.') {
+        currentInput = value;
+    } else {
+        currentInput += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '0';
+    updateDisplay();
+}
+
+function deleteLast() {
+    if (currentInput.length > 1) {
+        currentInput = currentInput.slice(0, -1);
+    } else {
+        currentInput = '0';
+    }
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        updateDisplay();
+    } catch (error) {
+        currentInput = 'Error';
+        updateDisplay();
+        setTimeout(() => {
+            currentInput = '0';
+            updateDisplay();
+        }, 1500);
+    }
+}
